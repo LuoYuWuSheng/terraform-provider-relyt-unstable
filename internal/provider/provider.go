bf106ef1e754ed185b35a226b3bd82fe641917cb
qingdeng <qingdeng@zbyte-inc.com>
2024-10-14 19:58:01 +0800

    support set http client TimeOutSecond

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -112,6 +112,10 @@ func (p *RelytProvider) Schema(ctx context.Context, req provider.SchemaRequest, 
 				Optional:    true,
 				Description: "Interval second used in wait for cycle check! Defaults 5",
 			},
+			"client_timeout": schema.Int64Attribute{
+				Optional:    true,
+				Description: "http client timeout seconds! Defaults 10",
+			},
 			"data_access_config": schema.SingleNestedAttribute{
 				Optional:    true,
 				Description: "data_access_configs",
@@ -189,6 +193,7 @@ func (p *RelytProvider) Configure(ctx context.Context, req provider.ConfigureReq
 	}
 	resourceWaitTimeout := int64(1800)
 	checkInterval := int32(5)
+	clientTimeout := int32(10)
 	if !data.ResourceCheckTimeout.IsNull() {
 		tflog.Info(ctx, "resource check wait isn't null! set value:"+strconv.FormatInt(data.ResourceCheckTimeout.ValueInt64(), 10))
 		resourceWaitTimeout = data.ResourceCheckTimeout.ValueInt64()
@@ -202,6 +207,13 @@ func (p *RelytProvider) Configure(ctx context.Context, req provider.ConfigureReq
 			resp.Diagnostics.AddAttributeError(path.Root("resource_check_interval"), "invalid value", "should be grater than 5")
 		}
 		checkInterval = int32(data.ResourceCheckInterval.ValueInt64())
+	}
+	if !data.ClientTimeout.IsNull() {
+		tflog.Info(ctx, "client timeout isn't null! set value:"+strconv.FormatInt(data.ClientTimeout.ValueInt64(), 10))
+		if data.ResourceCheckInterval.ValueInt64() <= 1 || data.ResourceCheckInterval.ValueInt64() >= math.MaxInt32 {
+			resp.Diagnostics.AddAttributeError(path.Root("client_timeout"), "invalid value", "should be grater than 1")
+		}
+		checkInterval = int32(data.ClientTimeout.ValueInt64())
 	}
 
 	if resp.Diagnostics.HasError() {
@@ -220,6 +232,7 @@ func (p *RelytProvider) Configure(ctx context.Context, req provider.ConfigureReq
 		Role:          roleId,
 		CheckTimeOut:  resourceWaitTimeout,
 		CheckInterval: checkInterval,
+		ClientTimeout: clientTimeout,
 	}
 	if data.DataAccessConfig != nil {
 		clientConfig.RelytDatabaseClientConfig = &client.RelytDatabaseClientConfig{
@@ -228,8 +241,13 @@ func (p *RelytProvider) Configure(ctx context.Context, req provider.ConfigureReq
 			SecretKey:     data.DataAccessConfig.SecretKey.ValueString(),
 			ClientTimeout: 10,
 		}
-		if !data.DataAccessConfig.ClientTimeout.IsNull() && !data.DataAccessConfig.ClientTimeout.IsUnknown() {
-			clientConfig.RelytDatabaseClientConfig.ClientTimeout = data.DataAccessConfig.ClientTimeout.ValueInt32()
+		if !data.DataAccessConfig.ClientTimeout.IsUnknown() {
+			dataAccessClientTimeOut := data.DataAccessConfig.ClientTimeout.ValueInt32()
+			if dataAccessClientTimeOut <= 0 {
+				resp.Diagnostics.AddError("wrong data_access_config config!", " client_timeout must greater than 0")
+				return
+			}
+			clientConfig.RelytDatabaseClientConfig.ClientTimeout = dataAccessClientTimeOut
 		}
 	}
 	relytClient, err := client.NewRelytClient(clientConfig)
