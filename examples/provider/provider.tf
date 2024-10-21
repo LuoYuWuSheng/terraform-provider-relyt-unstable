a05b1d2a5204465f08f6899fda64d9d9482bceda
qingdeng <qingdeng@zbyte-inc.com>
2024-10-21 11:35:31 +0800

    tmp

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -11,3 +11,11 @@ provider "relyt" {
   auth_key = "9a3727e5b9c0mockaGbll2HVLVKLLY1AyjOilAqeyPOBAb74A7VlMOCKTi0bJWJd3"
   role     = "SYSTEMADMIN"
 }
+
+#provider to operate database and schema
+
+provider "relyt" {
+  alias    = "database"
+  auth_key = "9a3727e5b9c0mockaGbll2HVLVKLLY1AyjOilAqeyPOBAb74A7VlMOCKTi0bJWJd3"
+
+}
