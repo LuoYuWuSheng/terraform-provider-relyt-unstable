444122c9062ece108a28e2422d30bc9355128843
qingdeng <qingdeng@zbyte-inc.com>
2024-12-26 21:00:35 +0800

    dwsu user security policy

--- docs/demo/terraform/modules/relyt/dw_user/main.tf
+++ docs/demo/terraform/modules/relyt/dw_user/main.tf
@@ -10,12 +10,6 @@ provider "relyt" {
   role     = var.role
 }
 
-resource "relyt_dwsu_user_policy" "security_constraints" {
-  dwsu_id             = var.dwsu_id
-  mfa                 = "OPTIONAL"
-  reset_init_password = true
-}
-
 resource "relyt_dwuser" "dw_user1" {
   dwsu_id                                  = var.dwsu_id
   account_name                             = var.account_name
@@ -23,7 +17,6 @@ resource "relyt_dwuser" "dw_user1" {
   datalake_aws_lakeformation_role_arn      = var.datalake_aws_lakeformation_role_arn
   async_query_result_location_prefix       = var.async_query_result_location_prefix
   async_query_result_location_aws_role_arn = var.async_query_result_location_aws_role_arn
-  depends_on = [relyt_dwsu_user_policy.security_constraints]
 }
 
 data "relyt_dwsu_boto3_access_info" "boto3" {
